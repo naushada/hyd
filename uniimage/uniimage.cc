58656e93598be0504cfa1d3c96bf8c4a5e5d133f
naushada <naushad.dln@gmail.com>
2023-07-11 20:28:13 +0530

    Updated

--- uniimage/uniimage.cc
+++ uniimage/uniimage.cc
@@ -148,6 +148,25 @@ std::int32_t noor::Uniimage::start(std::int32_t toInMilliSeconds) {
             continue;
         } else if(!nReady) {
             //Timeout happens
+            auto &svc = GetService(noor::ServiceType::Tcp_Device_Client_Service_Async);
+            auto channel = svc->handle();
+            if(noor::client_connection::Disconnected == svc->connected_client(channel)) {
+                auto IP = svc->ip();
+                auto PORT = svc->port();
+                DeRegisterFromEPoll(channel);
+                DeleteService(noor::ServiceType::Tcp_Device_Client_Service_Async, channel);
+                CreateServiceAndRegisterToEPoll(noor::ServiceType::Tcp_Device_Client_Service_Async, IP, PORT, true);
+            }
+
+            auto &inst = GetService(noor::ServiceType::Tcp_Device_Console_Client_Service_Async);
+            channel = inst->handle();
+            if(noor::client_connection::Disconnected == svc->connected_client(channel)) {
+                auto IP = inst->ip();
+                auto PORT = inst->port();
+                DeRegisterFromEPoll(channel);
+                DeleteService(noor::ServiceType::Tcp_Device_Console_Client_Service_Async, channel);
+                CreateServiceAndRegisterToEPoll(noor::ServiceType::Tcp_Device_Console_Client_Service_Async, IP, PORT, true);
+            }
             continue;
         }
 
