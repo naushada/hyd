eecd2cb63cddcd1aefa3044fdb5005d98cc638e5
naushada <naushad.dln@gmail.com>
2023-07-13 23:08:09 +0530

    Updated

--- uniimage/http.hpp
+++ uniimage/http.hpp
@@ -32,11 +32,11 @@ class noor::Http {
                     
                     if(!m_header.compare(0, 8, "HTTP/1.1")) {
                         //this is a response.
-                        auto res = m_header.find_first_of(" ", 9);
-                        if(res != std::string::npos) {
-                            m_status_code = m_header.substr(9, res);
+                        //auto res = m_header.find_first_of(' ', 9);
+                        //if(res != std::string::npos) {
+                            m_status_code = m_header.substr(9, 3);
                             std::cout << __TIMESTAMP__ << " line: " << __LINE__ << " status_code: " << m_status_code << std::endl;
-                        }
+                        //}
 
                         parse_header(in);
                         m_body = get_body(in);
