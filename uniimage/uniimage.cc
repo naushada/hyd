87d51f8639d24fb1bd0d74f5020490e8bd8695a5
naushada <naushad.dln@gmail.com>
2023-07-13 13:51:53 +0530

    Updated

--- uniimage/uniimage.cc
+++ uniimage/uniimage.cc
@@ -542,7 +542,7 @@ std::int32_t noor::Uniimage::start(std::int32_t toInMilliSeconds) {
                                     json jobj = json::object();
                                     jobj["devices"] = jarray;
                                     //body = jobj.dump();
-                                    ss << "{\"devices\": [" << jarray.dump() << "]}";
+                                    ss << "{\"devices\": " << jarray.dump() << "}";
                                     
                                     auto resp = svc->buildHttpResponse(http, ss.str());
                                     auto ret = svc->web_tx(Fd, resp);
